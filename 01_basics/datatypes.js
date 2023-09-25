e3f7f51826b4bcadeaf886ede8c501a5b1a0e4b4
Shankar Gavit <110783843+Shankar5555@users.noreply.github.com>
2023-09-25 14:46:48 +0000

    dadatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+// Primitive  datatypes :- 
+
+//string = "Demo"
+//interger = 2551
+//float = 25.105
+//boolean = True/false
+// null  = standalone value (object)
+// undefined = not assign value or string 
+// symbol
+
+
+// Object
+
+// Array
+
+let name = "hello"
+let age = 36
+let isLoggedIn = true;
+
+console.log(typeof "shankar");    // chack datatype
---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+
+
+var accountPassword = "12345" // not to use var becs block and 
+accountCity = "Pune"   // not use as that method
+
+const accountId=1445223
+// accountId = 2  // not allowed to change const values
+
+let accountEmail = "jmsgavit@gmail.com"
+
+let accountState;   // undefined shows
+
+console.log(accountId);
+console.log([accountEmail,accountPassword,accountCity,accountState]);
