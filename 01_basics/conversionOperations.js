d069f7b0cf676fa22ce7be36a75a173a07021af1
Shankar Gavit <110783843+Shankar5555@users.noreply.github.com>
2023-09-25 16:10:51 +0000

    conversionof datatype

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,25 @@
+let scope = 33
+//console.log(typeof scope);
+
+let scope2 = "50"
+let valueInNumber = Number(scope2)
+//console.log(typeof valueInNumber);
+
+let scope3 = "254abc"
+let valueInNumber2 = Number(scope3)
+// console.log(typeof valueInNumber2); // dont use as like bcs it can be show as number
+//console.log(valueInNumber2);    //NaN
+
+let someNumber = 66
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+// "33"  = 33
+// "252asda" = NaN
+// true = 1 , fales = 0 
+// "" = false
+
+
