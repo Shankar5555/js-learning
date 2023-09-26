1ed056df628322f84d31009ddda18be800523dc1
Shankar Gavit <110783843+Shankar5555@users.noreply.github.com>
2023-09-26 06:56:53 +0000

    string

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,16 @@
+const name = "Shankar Gavit";
+let mobNumber = "92848810"
+
+console.log(name+mobNumber);
+
+console.log(name+mobNumber + " india");   // concatinate 
+
+console.log(`Hello! My Name is ${name} and My Mobile Number is ${mobNumber}`);
+
+
+
+// method two to 
+
+const gameName = new String('MaxPyan')
+let result = gameName.toUpperCase(gameName)
+console.log(result);
