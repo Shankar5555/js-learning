4b2e6027556c54414845934ddb147bf4b07e195e
Shankar Gavit <110783843+Shankar5555@users.noreply.github.com>
2023-09-26 06:04:51 +0000

    datatype summey

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,9 +1,8 @@
 // Primitive  datatypes :- 
 
-//string = "Demo"
-//number = 2551
-//float = 25.105
-//boolean = True/false
+// string = "Demo"
+// number = 2551  //float = 25.105
+// boolean = True/false
 // null  = standalone value (object)
 // undefined = not assign value or string 
 // symbol
@@ -11,14 +10,48 @@
 
 // refereance type (Non primitive)
 
-// Objects
-// Array
-// functions
+// Objects :- 
+// example : 
+/*
+let myObj = {
+     name: "shankar"
+     age: 22,
+ }
 
+*/ 
+
+
+// Array   => 
+// const arry = ["shaknar","umesh","Mahesh"]
+
+// functions :- 
+/*
+const myFunction = function () {
+    console.log("hello world");
+    
+}
+*/
+
+
+// typeof operator Result 
+
+// Type value :-   
+// Null = "Object "
+// Boolean = "boolean "
+// Number = "Number "
+// String = "String "
+// Symbol = "symbol"
+// function = "function" // object function
+// Object (native or host does implement [[Call]]) = "function"
+// Object (native and does not implement [[Call]]) = "object"
+// Object (host and does implement [[Call]]) = "undefined","boolean","number","or String"
 
 
 let name = "hello"
 let age = 36
+const isNum = 15.6;
 let isLoggedIn = true;
+const is = Symbol('123')
+// const bigNumber = 1235434355454545n
 
 console.log(typeof "shankar");    // chack datatype
